992896b873dd911375835de4ab9f6d239c1367f1
RedCommander735 <40245965+RedCommander735@users.noreply.github.com>
2025-08-03 00:00:37 +0200

    feat: add icon

---
+++ ui/src-tauri/gen/android/app/src/main/java/com/badgemagic_rs/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.badgemagic_rs
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
